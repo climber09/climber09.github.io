0473109fbd92052c5870c6d9317eaddf03577920
climber09 <hunter.james.p@gmail.com>
2026-03-22 23:50:22 -0700

    additions and modifications for locating posts by topics and search

---
+++ topic/topicInfo.js
@@ -0,0 +1,9 @@
+const topicInfo = {"java": {label: "Java/JEE", posts: ["p/jarfind","p/javadoc_viewer","p/jdbc_framework","p/jdbc_testing_with_mock_objects","p/web_services_part_1","p/web_services_part_3","p/web_services_part_4","p/xslt_for_dynamic_content_generation","p/xslt_with_jstl"]},
+"jdbc": {label: "JDBC", posts: ["p/jdbc_framework","p/jdbc_testing_with_mock_objects"]},
+"jsp": {label: "JSP", posts:["p/xslt_with_jstl"]},
+"jstl": {label: "JSTL", posts:["p/xslt_with_jstl"]},
+"xslt": {label: "XSLT", posts:["p/xslt_for_dynamic_content_generation","p/xslt_with_jstl"]},
+"javascript": {label: "JavaScript", posts:["p/dynamic_dom_viewer","p/javadoc_viewer"]},
+"perl": {label: "Perl", posts:["p/find_jars_perl_version","p/web_services_part_2"]},
+"testing": {label: "Testing", posts:["p/jdbc_testing_with_mock_objects"]},
+"webservices": {label: "Web Services", posts:["p/web_services_part_1","p/web_services_part_2","p/web_services_part_3","p/web_services_part_4"]}}
